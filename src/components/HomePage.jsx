a412e34ed0d66bff5db86b0f61ab468613f8555d
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-09-03 17:06:58 +0330

    HeroSection version 1

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Quiz App</h1>
-        <p className="text-lg mb-4">Test your knowledge with our fun quiz!</p>
+      <div className="text-center bg-gray-200 fixed top-11 left-0 w-full z-10 py-60">
+        <h1 className="text-5xl font-bold mb-5">Welcome to the Brief</h1>
+        <p className=" text-red-500 font-bold font-mono mb-10"> Summary of the 2025 Cypress Conference in 1 minute!</p>
+        <p className="text-lg mb-4">Test your Playwright knowledge with our fun quiz!</p>
         <Link
           to="/PlaywrightQuiz"
           className="bg-blue-500 text-white py-2 px-4 rounded-md"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 function Navbar() {
     return (
-      <nav className="bg-slate-300 fixed top-0 left-0 w-full z-10">
+      <nav className="bg-slate-100 fixed top-0 left-0 w-full z-10 font-semibold">
         <div className="flex justify-center py-2">
           <ul className="flex space-x-10">
-            <li><a href="#" className="text-gray-700">Cypress Conf</a></li>
-            <li><a href="#" className="text-gray-700">Playwright Conf</a></li>
+            <li><a href="#" className="text-gray-700">Conferences</a></li>
             <li><a href="#" className="text-gray-700">Try Yourself</a></li>
             <li><a href="#" className="text-gray-700">Register</a></li>
+            <li><a href="#" className="text-gray-700">Contribute</a></li>
           </ul>
         </div>
       </nav>
