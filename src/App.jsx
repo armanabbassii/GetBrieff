c321e3705e73ca60f00e3cf2ada5752a1f86ed10
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-09-03 16:10:08 +0330

    complete Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import QuestionBox from "./components/QuestionBox";
 import { QuizProvider } from "./context/QuizContext";
-import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <QuizProvider>
       <Router>
+        <Navbar />
         <div className="flex mx-auto w-full items-center justify-center my-20 max-w-2xl">
           <Routes>
             {/* Route for Home Page */}
-            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/"
+              element={
+                <>
+                  <HomePage />
+                </>
+              }
+            />
 
             {/* Route for Quiz Page */}
             <Route path="/PlaywrightQuiz" element={<QuestionBox />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-
-function Navbar (){
+function Navbar() {
     return (
-        <nav className="bg-white shadow-md fixed top-0 w-full z-10">
-            <div className="countainer mx-auto flex items-center justify-between py-4 px-6">
-            {/* <img src="logo.png" alt="Logo" className="h-8" /> */}
-            <ul className="flex space-x-6">
-                <li><a href="#" className="text-gray-700">store</a></li>
-                <li><a href="#" className="text-gray-700">Mac1</a></li>
-                <li><a href="#" className="text-gray-700">Mac2</a></li>
-                <li><a href="#" className="text-gray-700">Mac3</a></li>
-            </ul>
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+      <nav className="bg-slate-300 fixed top-0 left-0 w-full z-10">
+        <div className="flex justify-center py-2">
+          <ul className="flex space-x-10">
+            <li><a href="#" className="text-gray-700">Cypress Conf</a></li>
+            <li><a href="#" className="text-gray-700">Playwright Conf</a></li>
+            <li><a href="#" className="text-gray-700">Try Yourself</a></li>
+            <li><a href="#" className="text-gray-700">Register</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
