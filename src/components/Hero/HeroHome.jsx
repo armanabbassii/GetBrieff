e3ecf774c82922a9cf93ee6dd0b5724b311c3487
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-10-21 23:11:00 +0330

    change mainpage

--- src/components/Hero/HeroHome.jsx
+++ src/components/Hero/HeroHome.jsx
@@ -4,13 +4,11 @@ import { Link } from "react-router-dom";
 function HeroHome() {
   return (
     <div className="hero-section py-20">
-      <h1 className="text-5xl font-bold mb-5">Welcome to the Brrrief</h1>
+      <h1 className="text-5xl font-bold mb-5">Welcome to the GetBrief</h1>
       <p className="text-red-500 font-bold font-mono mb-10">
         Summary of the 2025 Cypress Conference in 1 minute!
       </p>
-      <p className="text-lg mb-4">
-        Test your Playwright knowledge with our fun quiz!
-      </p>
+      <p className="text-lg mb-4"></p>
       {/* <Link
         to="/PlaywrightQuiz"
         className="bg-blue-500 text-white py-2 px-4 rounded-md"
