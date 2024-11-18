61f1e8d41fe6beba7b084d728793397f95ec29c8
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-11-18 09:00:42 +0330

    imrpove conferences box

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -12,7 +12,7 @@ function Conferences() {
         HubSpot Heroes programming on our YouTube channel.
       </p>
 
-      <div className="mt-10 bg-white border border-gray-300 rounded-md p-6 mx-auto max-w-lg shadow-lg">
+      <div className="mt-10 bg-white border border-gray-300 p-10 mx-auto max-w-4xl shadow-lg">
         <h2 className="text-xl font-semibold text-gray-700">Report</h2>
         <h3 className="text-2xl font-bold text-red-700 mt-2">
           CYPRESS Conf 2024
@@ -28,21 +28,22 @@ function Conferences() {
         </p>
         <Link
           to="/conference-details"
-          className="inline-block mt-4 px-6 py-2 bg-blue-500 text-white rounded-md font-semibold"
+          className="inline-block mt-6 px-6 py-2 bg-blue-500 text-white font-semibold"
         >
           See More
         </Link>
       </div>
 
-      <div className="mt-8 flex justify-center space-x-4">
+      {/* <div className="mt-8 flex justify-center space-x-4">
         <button className="text-2xl">⬅️</button>
         <button className="text-2xl">➡️</button>
-      </div>
-
-      <div className="mt-4">
+      </div> */}
+      {/* Divider Line */}
+      <hr className="border-t-2 border-gray mt-10 mb-6 mx-auto max-w-4xl" />
+      <div className="flex justify-center mt-10">
         <Link
           to="/more-conferences"
-          className="bg-yellow-400 px-4 py-2 rounded-md font-semibold text-black"
+          className="bg-yellow-400 px-4 py-3 font-semibold text-black"
         >
           Show More Conferences
         </Link>
