6802b2f66891782d67b2669313a131c5a6da9042
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-11-19 08:59:22 +0330

    create conf constants

---
+++ src/constants/ConferencesBox.js
@@ -0,0 +1,25 @@
+  // Array of conference data
+  export const conferences = [
+    {
+      title: "CYPRESS Conf 2024",
+      description: [
+        "1. About conference: Get inspired by the work of millions of top-rated designers & agencies around the world. Automatically run them in your continuous integration builds.",
+        "2. Agencies around the world. Automatically run them in your continuous integration builds.",
+      ],
+    },
+    {
+      title: "React Summit 2024",
+      description: [
+        "1. Learn about the latest React features and best practices from top experts.",
+        "2. Collaborate with other developers to build innovative solutions.",
+      ],
+    },
+    {
+      title: "JavaScript Conf 2024",
+      description: [
+        "1. Explore the future of JavaScript and its ecosystem.",
+        "2. Gain hands-on experience with cutting-edge tools and frameworks.",
+      ],
+    },
+  ];
+  
