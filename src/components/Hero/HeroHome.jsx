cfeac3b4a613eb819efa095dcb97c43e92249dce
Arman Abbasi <arman.abbasi.nt@gmail.com>
2024-11-23 22:16:48 +0330

    solve the full width of the all parts

--- src/components/Hero/HeroHome.jsx
+++ src/components/Hero/HeroHome.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function HeroHome() {
   return (
-    <section className="bg-black text-center mt-8 py-32 text-white border border-gray-500">
+    <section className="bg-black text-center mt-14 py-32 text-white border border-gray-500">
       <h1 className="text-5xl font-bold">
         <span className="text-red-500">GetBrief.</span> Conferences. Articles.
       </h1>
